f47b2ce9d2c2b3cdd144dcf1c8d0c402440c52f8
ozennselin <129550025+ozennselin@users.noreply.github.com>
2026-01-14 22:19:32 +0300

    Add project files.

---
+++ AxionTech.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AxionTech.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
