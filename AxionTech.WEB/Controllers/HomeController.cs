95daf71aa7648c4a088fff306569cfbdf01e6fef
ozennselin <129550025+ozennselin@users.noreply.github.com>
2026-01-28 21:05:04 +0300

    layout,templete giydirmesi yapıldı.homeindex,productlist UI tarafı yapıldı

--- AxionTech.WEB/Controllers/HomeController.cs
+++ AxionTech.WEB/Controllers/HomeController.cs
@@ -11,15 +11,6 @@ namespace AxionTech.WEB.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+   
     }
 }
---
+++ AxionTech.WEB/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AxionTech.WEB.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
