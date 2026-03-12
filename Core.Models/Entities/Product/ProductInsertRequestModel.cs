5c1a59b5044e5a1e774a585d2bc16732a370d9c2
resulturfent <rsltrfnt@gmail.com>
2026-03-12 16:46:30 +0300

    bugfix

---
+++ Core.Models/Entities/Product/ProductInsertRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Models.Entities.Product;
+
+public class ProductInsertRequestModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Stock { get; set; }
+    public double Price { get; set; }
+}
