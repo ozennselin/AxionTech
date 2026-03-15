0ef3c59c706a7bfa7403cf4e6e8d28fac636dfe6
ozennselin <129550025+ozennselin@users.noreply.github.com>
2026-03-16 01:27:47 +0300

    productdocument ve productpicture için api controller katmanı eklendi

--- AxionTech.API/Controllers/ProductDocumentController.cs
+++ AxionTech.API/Controllers/ProductDocumentController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Business.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AxionTech.API.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
+
 public class ProductDocumentController : BaseAPIController
 {
-    public IActionResult Index()
+    private readonly IProductDocumentService _productDocumentService;
+    public ProductDocumentController(IProductDocumentService productDocumentService)
+    {
+        _productDocumentService = productDocumentService;
+    }
+    [HttpGet("GetByProductId")]
+    public IActionResult GetByProductId(int Id)
+    {
+        var getDocument = _productDocumentService.GetByProductId(Id);
+        return ResultAPI(getDocument);
+    }
+    public IActionResult List()
     {
         return Ok();
     }
--- AxionTech.API/Controllers/ProductPictureController.cs
+++ AxionTech.API/Controllers/ProductPictureController.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Business.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AxionTech.API.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
+
 public class ProductPictureController : BaseAPIController
 {
+    private readonly IProductPictureService _productPictureService;
+    public ProductPictureController(IProductPictureService productPictureService)
+    {
+        _productPictureService = productPictureService;
+    }
+    [HttpGet("GetByProductId")]
+    public IActionResult GetByProductId(int Id)
+    {
+        var getPicture = _productPictureService.GetByProductId(Id);
+        return ResultAPI(getPicture);
+    }
+
     public IActionResult Index()
     {
         return Ok();
