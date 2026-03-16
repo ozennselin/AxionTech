bdfd314f0e9c541db7a091be5fd1ff1c0e2cb54f
resulturfent <rsltrfnt@gmail.com>
2026-03-16 23:01:30 +0300

    Product için Admin panelinde Create, Detail işlemleri yapıldı, devam edilecek

--- AxionTech.WEB/Areas/AdminPanel/Controllers/ProductAPController.cs
+++ AxionTech.WEB/Areas/AdminPanel/Controllers/ProductAPController.cs
@@ -1,4 +1,5 @@
 ﻿using AxionTech.WEB.GetApi;
+using Core.Models.Entities.Product;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AxionTech.WEB.Areas.AdminPanel.Controllers;
@@ -6,16 +7,47 @@ namespace AxionTech.WEB.Areas.AdminPanel.Controllers;
 public class ProductAPController : Controller
 {
     private readonly ProductApi _productApi;
+        private readonly ProductPriceApi _productPriceApi;
+        private readonly ProductPictureApi _productPictureApi;
+        private readonly ProductDocumentApi _productDocumentApi;
 
-    public ProductAPController(ProductApi productApi)
+    public ProductAPController(ProductApi productApi, ProductPriceApi productPriceApi = null, ProductPictureApi productPictureApi = null, ProductDocumentApi productDocumentApi = null)
     {
         _productApi = productApi;
+        _productPriceApi = productPriceApi;
+        _productPictureApi = productPictureApi;
+        _productDocumentApi = productDocumentApi;
     }
 
     public IActionResult List()
     {
-        var list=_productApi.List();
+        var list = _productApi.List();
         return View(list);
+    }
+
+    public IActionResult Detail(int id)
+    {
+        var product = _productApi.GetById(id);
+
+        var price = _productPriceApi.GetPriceByProductId(id);
+
+        var getProductDetail = new ProductDetailResponseModel
+        {
+            ProductDetail = product,
+            //ProductPicture = _productPictureApi.List().Where(k=>k.ProductId==Id).ToList(),
+            //ProductDocument = _productDocumentApi.List().Where(k=>k.ProductId==Id).ToList(),
+            ProductDocument = null,
+            ProductPicture = null,
+            ProductPrice = price,
+
+        };
+        return View(getProductDetail);
+    }
+
+
+    public IActionResult Create()
+    {
+        return View();
     }
 }
 
