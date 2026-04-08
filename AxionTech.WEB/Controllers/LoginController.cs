6e99fbb9efc42d6232e1789b8278c6c091942c44
resulturfent <rsltrfnt@gmail.com>
2026-04-08 22:15:49 +0300

    login arayüzü yapıldı

---
+++ AxionTech.WEB/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AxionTech.WEB.Controllers;
+
+public class LoginController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Login(string user)
+    {
+        return View();
+    }
+}
---
+++ Core.Models/Entities/User/UserLoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models.Entities.User;
+
+public class UserLoginModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+}
