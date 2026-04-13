8ff43df049004500b3c49732a58f590d6f87ee4e
ozennselin <129550025+ozennselin@users.noreply.github.com>
2026-04-13 23:22:17 +0300

    popup sepet için prop eklendı

---
+++ Core.Models/Entities/CartItem/CartItemToggleDetailResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Entities.CartItem;
+
+public class CartItemToggleDetailResponseModel
+{
+    public int CartId { get; set; }
+    public int ProducItd { get; set; }
+    public string ProductName { get; set; }
+    public string ProductPicture { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal Price { get; set; }//Price = UnitPrice * Quantity
+    public decimal Total { get; set; }
+}
