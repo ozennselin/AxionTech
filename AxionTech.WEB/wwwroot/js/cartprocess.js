eab83b64871ce9a7771e9443ac487652648d9713
resulturfent <rsltrfnt@gmail.com>
2026-04-23 21:06:16 +0300

    sepete aynı ürün eklenince miktar 1 artırılma işlemi yapıldı(childNode[0])

--- AxionTech.WEB/wwwroot/js/cartprocess.js
+++ AxionTech.WEB/wwwroot/js/cartprocess.js
@@ -11,10 +11,10 @@
 
             list.forEach(function (item) {
 
-                        var eklenecekUrun = `<li>
+                var eklenecekUrun = `<li id="product${item.productId}">
                                     <a href="#" class="photo"><img src="${item.picture}" class="cart-thumb" alt="" /></a>
-                                    <h6><a href="#">${item.name} </a></h6>
-                                    <p class="${item.productId}">${item.quantity}x - <span class="price">${item.price}</span></p> </li>`;
+                                    <h6><a href="#">${item.productname} </a></h6>
+                                    <p id="quantity${item.productId}">${item.quantity}x - <span class="price">${item.price}</span></p> </li>`;
 
                         document.getElementById("cartDetail").insertAdjacentHTML("beforeend", eklenecekUrun);
                         var getTotal = document.getElementById("totalProcess");
@@ -59,56 +59,56 @@ function AddCart(productId) {
 
             if (gelenCevap.success) {
 
-                /*
-                var total = 0;
-                gelenCevap.data.foreach(function (item) {
+                if (gelenCevap.data.id) {
 
+                    try {//
 
-                    var eklenecekUrun = `<li>
-                                    <a href="#" class="photo"><img src="${item.picture}" class="cart-thumb" alt="" /></a>
-                                    <h6><a href="#">${item.name} </a></h6>
-                                    <p>${item.quantity}- <span class="price">${item.price}</span></p> </li>`;
-                    document.getElementById("cartDetail").insertAdjacentHTML("beforeend", eklenecekUrun);
-                    total = total + item.price * item.quantity;
+                        var getSameProduct = document.getElementById("product" + gelenCevap.data.id + "");
 
-                });
+                        if (getSameProduct == undefined || getSameProduct==null) {
 
-                var newTotalTag = `<li class="total" id="deleteTag">
-                       <strong>Total</strong>: <span class="float-right" id="totalProcess">${getNewTotal}</span>
-                <a href="~/Cart/List" class="btn btn-default hvr-bounce-to-bottom btn-cart">SEPETE GİT</a> </li>`;
-                document.getElementById("cartDetail").insertAdjacentHTML("beforeend", newTotalTag);
-                */
-                // alert(gelenCevap.message);
-                // Sepet güncellendiğinde sayfayı yenile
-                //sepet kısmına +1  ve ürünü toggle kısmına ekle
-
-
-                if (gelenCevap.productId) {
-                    var eklenecekUrun = `<li>
+                            var eklenecekUrun = `<li id="product${gelenCevap.data.id}">
                                     <a href="#" class="photo"><img src="${gelenCevap.data.picture}" class="cart-thumb" alt="" /></a>
                                     <h6><a href="#">${gelenCevap.data.name} </a></h6>
-                                    <p class="${gelenCevap.productId}">1x - <span class="price">${gelenCevap.data.price}</span></p> </li>`;
+                                    <p id="quantity${gelenCevap.data.id}">1x - <span class="price">${gelenCevap.data.price}</span></p> </li>`;
 
-                    document.getElementById("cartDetail").insertAdjacentHTML("beforeend", eklenecekUrun);
-                    //${gelenCevap.data.total}
-                    //totalProcess=> ilkin bu sil
-                    //silmeden sonra yeniSekme değişkenini aşağıdaki gibi en sona ekle
-                    var getTotal = document.getElementById("totalProcess");
-                    var getTotalPrice = parseInt(getTotal.innerHTML);
-                    var getNewTotal = getTotalPrice + gelenCevap.data.price;
+                            document.getElementById("cartDetail").insertAdjacentHTML("beforeend", eklenecekUrun);
+                            //${gelenCevap.data.total}
+                            //totalProcess=> ilkin bu sil
+                            //silmeden sonra yeniSekme değişkenini aşağıdaki gibi en sona ekle
+                            var getTotal = document.getElementById("totalProcess");
+                            var getTotalPrice = parseInt(getTotal.innerHTML);
+                            var getNewTotal = getTotalPrice + gelenCevap.data.price;
 
-                    document.getElementById("deleteTag").remove();
+                            document.getElementById("deleteTag").remove();
 
-                    var newTotalTag = `<li class="total" id="deleteTag">
+                            var newTotalTag = `<li class="total" id="deleteTag">
                        <strong>Total</strong>: <span class="float-right" id="totalProcess">${getNewTotal}</span>
-                <a href="/Cart/List" class="btn btn-default hvr-bounce-to-bottom btn-cart">SEPETE GİT</a> </li>`;
+                         <a href="/Cart/List" class="btn btn-default hvr-bounce-to-bottom btn-cart">SEPETE GİT</a> </li>`;
 
-                    document.getElementById("cartDetail").insertAdjacentHTML("beforeend", newTotalTag)
+                            document.getElementById("cartDetail").insertAdjacentHTML("beforeend", newTotalTag)
 
-                    var getProductCount = document.getElementById("cartProductCount");
-                    var getCount = parseInt(getProductCount.innerHTML);
-                    getCount += 1;
-                    getProductCount.innerHTML = getCount;
+                            var getProductCount = document.getElementById("cartProductCount");
+                            var getCount = parseInt(getProductCount.innerHTML);
+                            getCount += 1;
+                            getProductCount.innerHTML = getCount;
+                        }
+
+                        else {
+                            var element = document.getElementById("quantity" + gelenCevap.data.id);
+                            var text = element.childNodes[0].nodeValue.trim(); // "2x -"
+                            var number = parseInt(text); // 2
+                            number = number + 1;
+                            document.getElementById("quantity" + gelenCevap.data.id).innerHTML = number + "x -";
+
+                        }
+
+                    } catch (e) {
+
+
+                    }
+
+                    
 
                 }
                 else {
