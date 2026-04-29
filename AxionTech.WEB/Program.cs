b0ca0285674515be14d68e608676fcc021c7fb1c
resulturfent <rsltrfnt@gmail.com>
2026-04-29 22:24:38 +0300

    sub kategory işlemleri yapıldı

--- AxionTech.WEB/Program.cs
+++ AxionTech.WEB/Program.cs
@@ -10,7 +10,7 @@ namespace AxionTech.WEB
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddNewtonsoftJson();
             builder.Services.AddSession();
             builder.Services.AddHttpClient();
 
