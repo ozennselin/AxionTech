a1a887fbb3501c05953aa4e5d7182c89fbc9787f
resulturfent <rsltrfnt@gmail.com>
2026-05-04 20:58:08 +0300

    Response Messgae tablo

---
+++ Data.Infrastructure/Entities/ResponseMessage.cs
@@ -0,0 +1,13 @@
+﻿using Data.Infrastructure.Abstraction;
+
+namespace Data.Infrastructure.Entities;
+
+public class ResponseMessage:BaseEntity
+{
+    public string Key { get; set; }//Enum için string yapıya karşılık gelecek//Success
+    public string MessageTR { get; set; }//Başarılı
+    public string MessageEng { get; set; }//Success
+    public string MessageDautch { get; set; }//S
+    public string MessageDautKrt { get; set; }//Serqefti
+
+}
