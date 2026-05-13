511213dc3658b049ecbbe9995c3e3eb0ebe4d3d5
ozennselin <129550025+ozennselin@users.noreply.github.com>
2026-05-13 19:15:28 +0300

    Admin panel dashboard sayfası oluşturuldu ve canlı istatistik verileri eklendi

---
+++ Core.Models/Entities/Dashboard/DashboardResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Models.Entities.Dashboard;
+
+public class DashboardResponseModel
+{
+    public int ProductCount { get; set; }
+    public int UserCount { get; set; }
+    public int OrderCount { get; set; }
+    public int CartCount { get; set; }
+}
