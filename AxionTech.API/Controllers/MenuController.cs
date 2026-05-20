3794373942d21130d18e5c9b4c8cce280492c060
ozennselin <129550025+ozennselin@users.noreply.github.com>
2026-05-20 22:26:36 +0300

    menu getapi ve apicontroller tarafı yapıldı

---
+++ AxionTech.API/Controllers/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Service.Interfaces;
+using Core.Enums;
+using Core.Models.Entities.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AxionTech.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MenuController : BaseAPIController
+{
+    private readonly IMenuService _menuService;
+
+    public MenuController(IMenuService menuService)
+    {
+        _menuService = menuService;
+    }
+
+    [HttpPost("Create")]
+    public IActionResult Create([FromBody]CreateMenuRequestModel request)
+    {
+        var result = _menuService.Create(request);
+        if (result.Item1 == Core.Enums.ResponseMessageEnum.Success)
+        {
+            return ResultAPI(result.Item2);
+        }
+        return BadRequest(new { Message = ResponseMessageEnum.ErrorWithData, ErrorCode = result.Item1 });
+    }
+
+     
+}
---
+++ AxionTech.WEB/GetApi/MenuApi.cs
@@ -0,0 +1,12 @@
+﻿using Core.Models.Entities.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AxionTech.WEB.GetApi;
+
+public class MenuApi
+{
+    private readonly HttpClient _httpClient;
+
+
+
+}
